3fa6e0e74dfae118ac9d1e23ac98f7f7366ade2e
richard <richard.marklund@gmail.com>
2019-09-20 09:59:41 +0200

    create shell files

---
+++ goodDeeds/View/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.goodpeople.gooddeeds.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.goodpeople.gooddeeds.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ goodDeeds/app/src/main/java/com/goodpeople/gooddeeds/Model/GoodDeeds.java
@@ -0,0 +1,17 @@
+package com.goodpeople.gooddeeds.Model;
+
+import com.goodpeople.gooddeeds.Model.Entities.Account;
+import com.goodpeople.gooddeeds.Model.Entities.Deeds;
+
+import java.util.List;
+
+public class GoodDeeds {
+
+    List<Deeds> offers;
+    List<Account> accounts;
+
+    public GoodDeeds() {
+    }
+
+
+}
