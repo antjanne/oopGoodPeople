0c7c334fd40aa1f64c78619c95a24c2f10dcea9e
richardmarklund <richard.marklund@gmail.com>
2019-09-20 10:09:37 +0200

    create first domain design files

---
+++ goodDeeds/app/src/main/java/com/goodpeople/gooddeeds/Controller/DeedController.java
@@ -0,0 +1,11 @@
+package com.goodpeople.gooddeeds.Controller;
+
+import com.goodpeople.gooddeeds.Model.GoodDeeds;
+
+public class DeedController {
+    GoodDeeds goodDeeds;
+
+    public DeedController() {
+        this.goodDeeds = new GoodDeeds();
+    }
+}
---
+++ goodDeeds/app/src/main/java/com/goodpeople/gooddeeds/View/MainActivity.java
@@ -0,0 +1,16 @@
+package com.goodpeople.gooddeeds.View;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.goodpeople.gooddeeds.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
