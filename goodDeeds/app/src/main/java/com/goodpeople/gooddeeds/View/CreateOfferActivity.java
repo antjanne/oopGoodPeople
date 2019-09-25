55d02530e468b6de3d423747c35af4494f1612a7
= <=>
2019-09-25 11:07:06 +0200

    add fields and buttons to activity_createoffer, create class CreateOfferActivity in View

---
+++ goodDeeds/app/src/main/java/com/goodpeople/gooddeeds/View/CreateOfferActivity.java
@@ -0,0 +1,20 @@
+package com.goodpeople.gooddeeds.View;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.goodpeople.gooddeeds.R;
+
+public class CreateOfferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Button cancelButton =
+}
