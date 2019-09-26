edceb983d914661539d43cad0a9c8714a905056f
loranta <lo.ranta86@gmail.com>
2019-09-26 10:31:54 +0200

    editOffer

--- goodDeeds/app/src/main/java/com/goodpeople/gooddeeds/Model/GoodDeeds.java
+++ goodDeeds/app/src/main/java/com/goodpeople/gooddeeds/Model/GoodDeeds.java
@@ -4,6 +4,7 @@ import com.goodpeople.gooddeeds.Model.Entities.Account;
 import com.goodpeople.gooddeeds.Model.Entities.Deed;
 
 import java.util.List;
+import java.util.UUID;
 
 public class GoodDeeds {
 
@@ -11,6 +12,17 @@ public class GoodDeeds {
     List<Account> accounts;
 
     public GoodDeeds() {
+    }
+
+    public void editOffer(UUID id, String subject, String description) {
+        for (int i = 0; i < offers.size(); i++ ) {
+            if (offers.get(i).getId() == id) {
+                offers.get(i).setSubject(subject);
+                offers.get(i).setDescription(description);
+
+            }
+        } System.out.println("Deed not found");
+
     }
 
 
